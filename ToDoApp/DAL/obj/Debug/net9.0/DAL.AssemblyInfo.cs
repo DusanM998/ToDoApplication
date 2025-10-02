118957ec8b20884bfa14ffef85905bd416c04484
DusanM998 <milojkovic.dusan98@gmail.com>
2025-10-02 15:03:09 +0200

    Update version 1.5

--- ToDoApp/DAL/obj/Debug/net9.0/DAL.AssemblyInfo.cs
+++ ToDoApp/DAL/obj/Debug/net9.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc82a52d6bf8058486cdfc6e90c447fe21776b34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae782873c42aa7d5cdfda28f946d462b113c6a8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
