dff8dc70545c142aca292587985a902c818ce97e
DusanM998 <milojkovic.dusan98@gmail.com>
2025-12-20 22:16:15 +0100

    Add to feature branch

---
+++ ToDoApp/BLL/Mapping/GroupProfile.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using EL.DTOs.GroupDTO;
+using EL.Models.Group;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Mapping
+{
+    public class GroupProfile : Profile
+    {
+        public GroupProfile()
+        {
+            // Mapping Group -> GroupDTO
+            CreateMap<Group, GroupDTO>()
+                .ForMember(dest => dest.Messages, opt => opt.MapFrom(src => src.Messages))
+                .ForMember(dest => dest.Members, opt => opt.MapFrom(src => src.Members));
+
+            // Mapping GroupMessage -> GroupMessageDTO
+            CreateMap<Group, GroupMessageDTO>()
+                .ForMember(dest => dest.SenderEmail, opt => opt.MapFrom(src => src.Sender.Email));
+
+            // GroupMember → GroupMemberDTO
+            CreateMap<GroupMember, GroupMemberDTO>()
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.User.Email));
+        }
+    }
+}
--- ToDoApp/ToDoApp/obj/Debug/net9.0/ToDoApp.AssemblyInfo.cs
+++ ToDoApp/ToDoApp/obj/Debug/net9.0/ToDoApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9e5b3162fe936570bbf2f60621f0958a91c60ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a30a1a91dc8bf8a47d6b49ef2e8cde3d19c4d1af")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
