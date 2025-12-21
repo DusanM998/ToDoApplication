6af44be5d447dda205e6cfb3ea437ef1e337fdb7
DusanM998 <milojkovic.dusan98@gmail.com>
2025-12-21 11:11:08 +0100

    Add AutoMapper

--- ToDoApp/BLL/Mapping/GroupProfile.cs
+++ ToDoApp/BLL/Mapping/GroupProfile.cs
@@ -15,16 +15,35 @@ namespace BLL.Mapping
         {
             // Mapping Group -> GroupDTO
             CreateMap<Group, GroupDTO>()
-                .ForMember(dest => dest.Messages, opt => opt.MapFrom(src => src.Messages))
-                .ForMember(dest => dest.Members, opt => opt.MapFrom(src => src.Members));
+                .ForMember(dest => dest.Messages,
+                    opt => opt.MapFrom(src => src.Messages))
+                .ForMember(dest => dest.Members,
+                    opt => opt.MapFrom(src => src.Members));
 
             // Mapping GroupMessage -> GroupMessageDTO
-            CreateMap<Group, GroupMessageDTO>()
-                .ForMember(dest => dest.SenderEmail, opt => opt.MapFrom(src => src.Sender.Email));
+            CreateMap<GroupMember, GroupMemberDTO>()
+                .ForMember(dest => dest.UserId,
+                    opt => opt.MapFrom(src => src.UserId))
+                .ForMember(dest => dest.Email,
+                    opt => opt.MapFrom(src => src.User.Email))
+                .ForMember(dest => dest.IsAdmin,
+                    opt => opt.MapFrom(src => src.IsAdmin));
 
             // GroupMember → GroupMemberDTO
-            CreateMap<GroupMember, GroupMemberDTO>()
-                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.User.Email));
+            CreateMap<GroupMessage, GroupMessageDTO>()
+                .ForMember(dest => dest.SenderId,
+                    opt => opt.MapFrom(src => src.SenderId))
+                .ForMember(dest => dest.SenderEmail,
+                    opt => opt.MapFrom(src => src.Sender.Email))
+                .ForMember(dest => dest.Content,
+                    opt => opt.MapFrom(src => src.Content))
+                .ForMember(dest => dest.SendAt,
+                    opt => opt.MapFrom(src => src.SendAt));
+
+            CreateMap<GroupMessageCreateDTO, GroupMessage>()
+                .ForMember(dest => dest.Content,
+                opt => opt.MapFrom(src => src.Content));
+
         }
     }
 }
